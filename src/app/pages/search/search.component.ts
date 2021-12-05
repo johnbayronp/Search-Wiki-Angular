f72383630d93affb198987a78d9b02963d34f513
johnbayronp <webdeprueba1.1@gmail.com>
2021-12-04 23:06:26 -0500

    2 files modified

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  inputSearch: FormControl;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.inputSearch.valueChanges.pipe(tap((e) => console.log(e))).subscribe();
   }
-
 }
