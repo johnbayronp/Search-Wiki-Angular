347b4d24861303ec6d737aaea81f6a019bb75044
johnbayronp <webdeprueba1.1@gmail.com>
2021-12-04 22:35:45 -0500

    4 files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './components/title.component';
+import { SearchModule } from './pages/search/search.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule,SearchModule],
   declarations: [AppComponent, TitleComponent],
   bootstrap: [AppComponent],
 })
