99b34302b12bab2275123929f5412533e16c5a3b
johnbayronp <webdeprueba1.1@gmail.com>
2021-12-09 13:27:28 -0500

    3 files modified

--- src/app/pokemon/pipes/filtro.pipe.ts
+++ src/app/pokemon/pipes/filtro.pipe.ts
@@ -5,7 +5,15 @@ import { Pokemon } from '../interfaces/pokemon.interface';
   name: 'filtro',
 })
 export class FiltroPipe implements PipeTransform {
-  transform(pokemons: Pokemon[], page: number): Pokemon[] {
-    return pokemons.slice(page, page + 5);
+  transform(pokemons: Pokemon[], page: number, search: string = ''): Pokemon[] {
+    if (pokemons.length === 0) return pokemons.slice(page, page + 5);
+    // slice te permite tomar dos partes desde un array posicion inicial y posicion final [0,1,2,3,4] => slice(0,5)
+
+    const filteredPokemons = pokemons.filter((poke) =>
+      poke.name.includes(search)
+    );
+    // .filter( obj => obj.name) , la funcion includes indica si ese string include alguna letra
+
+    return filteredPokemons.slice(page, page + 5);
   }
 }
